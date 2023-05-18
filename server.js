5ccbcf774d8eca9906f4af395d56ba43a004caac
sLrBastos <cerejobastosa7x@gmail.com>
2023-05-18 10:59:06 +0100

    create base of the server

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require("express")
+require("dotenv").config()
+const port = process.env.APP_PORT || 5001
+const server = express()
+server.unsubscribe(express.json())
+
+
+
+server.listen(port, () => {
+    console.log("server listening on port ", port)
+} )
